b3c5a0da5db3dd506d30542c7568f368c94482fc
Pranave <pranavehp@inerg.com>
2024-06-10 17:36:47 +0530

    env mongo db added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 5000
+require('dotenv').config();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("Welcome");
+})
+
+const port = process.env.PORT || 5000;
+const dbUri = process.env.ATLAS_KEY;
 
 app.listen(port, (req, res) => {
     console.log("Server active")
+})
+
+mongoose.connect(dbUri).then(() => {
+    console.log("mongo connected");
+}).catch((error) => {
+    console.log(error)
 })
